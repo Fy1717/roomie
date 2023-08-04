513396bbf016252cb44f415419ab09df045814ae
Furkan Yıldız <40992809+Fy1717@users.noreply.github.com>
2023-08-04 22:27:08 +0300

    bottom bar added

--- app/src/main/java/com/example/roomie/view/MainAreaPage.java
+++ app/src/main/java/com/example/roomie/view/MainAreaPage.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
@@ -15,6 +17,7 @@ import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
@@ -23,15 +26,21 @@ import com.example.roomie.R;
 import com.example.roomie.model.RoomDB.UserRoomController;
 import com.example.roomie.model.User;
 import com.example.roomie.service.Request.Logout;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationBarView;
+
 
 import java.util.ArrayList;
 
 
 public class MainAreaPage extends AppCompatActivity {
-    Button searchWordButton, rightMenuButton;
+    Button searchWordButton;
     User user = User.getInstance();
     //SwipeRefreshLayout pullToRefresh;
     UserRoomController userRoomController;
+    BottomNavigationView bottomNavigationView;
+    FloatingActionButton openSearchButton;
 
 
     @Override
@@ -44,8 +53,25 @@ public class MainAreaPage extends AppCompatActivity {
 
         //pullToRefresh = findViewById(R.id.swipeRefreshLayout);
 
-        rightMenuButton = findViewById(R.id.profile_button);
-        rightMenuButton.setOnClickListener(v -> showProfileDialog());
+        openSearchButton = findViewById(R.id.searchButton);
+        openSearchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showProfileDialog();
+            }
+        });
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setItemIconTintList(null);
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                System.out.println("ITEM_SELECTED" + item.getTitle());
+
+                return true;
+            }
+        });
     }
 
 
@@ -60,6 +86,8 @@ public class MainAreaPage extends AppCompatActivity {
                 closeButton.setOnClickListener(v -> dialog.dismiss());
 
                 Button exitButton = dialog.findViewById(R.id.exit_button);
+
+
                 exitButton.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
@@ -74,6 +102,8 @@ public class MainAreaPage extends AppCompatActivity {
                     }
                 });
 
+
+
                 TextView username = dialog.findViewById(R.id.username);
 
                 if (user.getUsername() != null) {
@@ -84,13 +114,14 @@ public class MainAreaPage extends AppCompatActivity {
 
                 Spinner dropdownCities, dropdownDistrict, dropdownNeighbourhood,
                         dropdownSmoke, dropdownAlcohol, dropdownGuest, dropdownAnimal,
-                        dropdownGender;
+                        dropdownGender, dropdownBudget;
 
                 ArrayAdapter<String> adapterOfCities, adapterOfDistrict, adapterOfNeighbourhood,
-                        adapterOfSmoke, adapterOfAlcohol, adapterOfGuest, adapterOfAnimal, adapterOfGender;
+                        adapterOfSmoke, adapterOfAlcohol, adapterOfGuest, adapterOfAnimal,
+                        adapterOfGender, adapterOfBudget;
 
                 String[] itemsCity, itemsDistrict, itemsNeighbourhood, itemsAnimal, itemsGuest, itemsGender,
-                        itemsSmoke, itemsAlcohol;
+                        itemsSmoke, itemsAlcohol, itemsBudget;
 
                 itemsCity = new String[6];
                 itemsCity[0] = "İl?";
@@ -160,6 +191,18 @@ public class MainAreaPage extends AppCompatActivity {
                 itemsSmoke[2] = "Evet";
                 itemsSmoke[3] = "Tabiki evet";
 
+                // --------------------------------
+
+                itemsBudget = new String[7];
+                itemsBudget[0] = "Ortalama bütçeniz ne kadar?";
+                itemsBudget[1] = "500-1500";
+                itemsBudget[2] = "1501-3000";
+                itemsBudget[3] = "3001-5000";
+                itemsBudget[4] = "5001-7500";
+                itemsBudget[5] = "7501-10000";
+                itemsBudget[6] = "10001- .....";
+
+
                 adapterOfCities = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, itemsCity);
                 adapterOfDistrict = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, itemsDistrict);
                 adapterOfNeighbourhood = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, itemsNeighbourhood);
@@ -168,6 +211,7 @@ public class MainAreaPage extends AppCompatActivity {
                 adapterOfAnimal = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, itemsAnimal);
                 adapterOfGuest = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, itemsGuest);
                 adapterOfSmoke = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, itemsSmoke);
+                adapterOfBudget = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, itemsBudget);
 
                 dropdownCities = dialog.findViewById(R.id.city);
                 dropdownDistrict = dialog.findViewById(R.id.district);
@@ -177,6 +221,7 @@ public class MainAreaPage extends AppCompatActivity {
                 dropdownGuest = dialog.findViewById(R.id.guest);
                 dropdownAnimal = dialog.findViewById(R.id.animal);
                 dropdownGender = dialog.findViewById(R.id.gender);
+                dropdownBudget = dialog.findViewById(R.id.budget);
 
                 dropdownCities.setAdapter(adapterOfCities);
                 dropdownDistrict.setAdapter(adapterOfDistrict);
@@ -186,6 +231,7 @@ public class MainAreaPage extends AppCompatActivity {
                 dropdownGuest.setAdapter(adapterOfGuest);
                 dropdownAnimal.setAdapter(adapterOfAnimal);
                 dropdownGender.setAdapter(adapterOfGender);
+                dropdownBudget.setAdapter(adapterOfBudget);
 
                 dialog.show();
                 dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
@@ -196,6 +242,8 @@ public class MainAreaPage extends AppCompatActivity {
             });
         } catch (Exception e) {
             e.printStackTrace();
+
+            Log.e("SEARCH_AREA", "Error : " + e.getMessage());
         }
     }
 
